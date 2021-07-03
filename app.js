23c6b22dfdf4726bb99acbf5f52b0bb226bca2e9
sae naya <>
2021-07-03 15:34:45 +0900

    package.jsonインストール

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-app.get('/', (req, res) => {
-    res.render("index.ejs",{message:"helloworld"});
-});
+// app.get('/', (req, res) => {
+//     res.render("index.ejs",{message:"helloworld"});
+// });
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`server started port ${port}`));
+// const port = process.env.PORT || 3000
+// app.listen(port, () => console.log(`server started port ${port}`));
+
+console.log("HEllo nodejs");
