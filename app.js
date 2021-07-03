6f35194b0cf4a1278da2516fbf190854d70a6388
sae naya <>
2021-07-03 15:40:34 +0900

    localhostでhelloworld

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-// app.get('/', (req, res) => {
-//     res.render("index.ejs",{message:"helloworld"});
-// });
+app.get('/', (req, res) => {
+    res.render("index.ejs",{message:"helloworld"});
+});
 
-// const port = process.env.PORT || 3000
-// app.listen(port, () => console.log(`server started port ${port}`));
-
-console.log("HEllo nodejs");
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`server started port ${port}`));
